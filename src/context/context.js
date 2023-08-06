03b388c0c546e208e13321077df519afb7f9fe10
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-08-06 13:31:08 +0400

    added scrypt for build in aws

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import defaultUser from "./mockData/defaultUser";
 const rootUrl = "https://api.github.com";
 
